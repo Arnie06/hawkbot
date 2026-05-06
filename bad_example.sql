7b391f33d2875e621e774f778096b57423fd72fe
Arnie06 <aaron.lane@live.com>
2026-05-05 20:05:22 -0400

    another

--- bad_example.sql
+++ bad_example.sql
@@ -11,7 +11,23 @@ user_src as (
     select *
     from raw.users
 )
--- try again
+-- anything wrong here
+
+select
+	o.id
+	,o.status
+	,o.amount
+	,u.name customer_name
+	,u.email customer_email
+	,case when o.amount > 100 then true else false end is_large_order
+	,o.created_at as order_date
+	,_rescued_data
+	,adduser
+	,adddatetime
+from order_src o
+join user_src u on o.uid = u.id
+where o.status = 'pending'
+
 select
     o.id
     ,o.status
@@ -28,18 +44,3 @@ right join user_src u on o.uid = u.id
 where o.status = 'active'
   and u.name is not null
 union
-
-select
-	o.id
-	,o.status
-	,o.amount
-	,u.name customer_name
-	,u.email customer_email
-	,case when o.amount > 100 then true else false end is_large_order
-	,o.created_at as order_date
-	,_rescued_data
-	,adduser
-	,adddatetime
-from order_src o
-join user_src u on o.uid = u.id
-where o.status = 'pending'
